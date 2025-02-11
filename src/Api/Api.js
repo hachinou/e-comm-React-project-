534a7b072d90d3ae693979675aaa10cc08058546
hachinou <mechmedamin@gmail.com>
2025-02-10 16:26:52 -0800

    update

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,4 +1,4 @@
-export const baseURL = `http://127.0.0.1:8000/api`;
+export const baseURL = `https://backend-data-production.up.railway.app/api`;
 // Auth
 export const REGISTER = "register";
 export const LOGIN = "login";
