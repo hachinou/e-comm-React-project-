31db241af439b7ebebd9af554cb2f762f135bb76
hachinou <mechmedamin@gmail.com>
2025-02-12 15:57:43 -0800

    update

--- src/Components/Dashboard/NavLink.js
+++ src/Components/Dashboard/NavLink.js
@@ -1,56 +1,45 @@
-
-import { faUsers, faPlus, faCartShopping,faTruckFast} from "@fortawesome/free-solid-svg-icons";
+import {
+  faUsers,
+  faPlus,
+  faCartShopping,
+  faTruckFast,
+} from "@fortawesome/free-solid-svg-icons";
 
 export const links = [
-    {
-        name: "Users",
-        path: "users",
-        icon:faUsers,
-        role:"1995",
-       
-
-    },
-    {
-        name: "Add Users",
-        path: "/dashboard/user/add",
-        icon:faPlus,
-        role:"1995",
-       
-        
-    },
-    {
-        name: "categories",
-        path: "/dashboard/categories",
-        icon:faCartShopping,
-        role:["1995","1999"],  
-    },
-    {
-        name: "Add Category",
-        path: "/dashboard/category/add",
-        icon:faPlus,
-        role:["1995","1999"],
-      
-    },
-    {
-        name: "Products",
-        path: "/dashboard/products",
-        icon:faTruckFast,
-        role:["1995","1999"],  
-    },
-    {
-        name: "Add Product",
-        path: "/dashboard/product/add",
-        icon:faPlus,
-        role:["1995","1999"],
-      
-    },
-    {
-        name: "Writer",
-        path: "/dashboard/writer",
-        icon:faPlus,
-        role:["1996","1995"],
-       
-        
-    },
-   
-]
+  {
+    name: "Users",
+    path: "users",
+    icon: faUsers,
+    role: "1995",
+  },
+  {
+    name: "Add Users",
+    path: "/dashboard/user/add",
+    icon: faPlus,
+    role: "1995",
+  },
+  {
+    name: "categories",
+    path: "/dashboard/categories",
+    icon: faCartShopping,
+    role: ["1995", "1999"],
+  },
+  {
+    name: "Add Category",
+    path: "/dashboard/category/add",
+    icon: faPlus,
+    role: ["1995", "1999"],
+  },
+  {
+    name: "Products",
+    path: "/dashboard/products",
+    icon: faTruckFast,
+    role: ["1995", "1999"],
+  },
+  {
+    name: "Add Product",
+    path: "/dashboard/product/add",
+    icon: faPlus,
+    role: ["1995", "1999"],
+  },
+];
